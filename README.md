3c9a3ff7f509d48e0c1474c4932310ea1d054387
jqdai <13524976297@163.com>
2020-01-16 20:08:37 +0800

    some preparations

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# snake
- 贪吃蛇怎么翻比较好听呢？
+# Snake
+
+贪吃蛇怎么翻比较好听呢？
+
+## 项目背景
+
+贪吃蛇是大伙的佬朋友了就8用多说了8
+
+## 开发环境
+
+- Windows 10 64位 ( DirectX 12 )
+
+## 贡献者
+
+- [Jiaqi Dai](https://github.com/jqdai) - Fudan University
---
+++ snake.cpp
@@ -0,0 +1,19 @@
+#include<algorithm>
+#include<iostream>
+#include<string>
+#include<time.h>
+#define m 80
+
+using namespace std;
+
+class Snake {
+public:
+    Snake();
+private:
+    int board[m][m];
+};
+
+int main()
+{
+    ;
+}
